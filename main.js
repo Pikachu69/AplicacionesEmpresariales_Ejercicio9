da10fb3238369708e52639be64f95e65682a0d0d
Eduardo <lalitro.es@gmail.com>
2019-02-28 13:13:01 -0700

    mongoose schema and conection to mongodb + save

---
+++ main.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var blogSchema = require('./schema');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/alumnos',{useNewUrlParser:true});
+
+var Blog = mongoose.model('Blog',blogSchema,"blogs");
+
+//Agregar blog
+
+var blog1 = new Blog({
+    title:'Hasta el moño del orgullo',
+    author:'Sarah Brenan',
+    body:'Decía una amiga de mi madre muy estilosona que el verano es la época más hortera del año.',
+    comments:[{body:'Regular', date:'1/07/2018'}],
+    meta: {votes:3, favs:4}
+});
+
+
+//Guardar
+
+blog1.save((error) => {
+    if(error){
+        console.log(error);
+        process.exit(1);
+    }
+    console.log("Guardado");
+    process.exit(0);
+});
